408f6eb080407de567d84b48b3d5dce3691bb9a2
Leonardo Risco <leo99153@gmail.com>
2024-02-22 16:35:43 -0500

    deploying front

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -4,7 +4,9 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store.js';
+import axios from 'axios';
 import './index.css'
+//Deploy
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store = {store}>
